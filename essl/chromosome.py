5e6a2d9ce3aa0b373f8243af4661e831e7c5c28f
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-04-26 13:23:45 -0300

    first implementation of fitness function

--- essl/chromosome.py
+++ essl/chromosome.py
@@ -2,10 +2,11 @@ import ops
 import random
 
 class chromosome_generator:
-    def __init__(self, augmentations=ops.DEFAULT_OPS, len=5):
+    def __init__(self, augmentations=ops.DEFAULT_OPS, length=5):
         """
         :param augmentations: dict containing operation, magnitude pairs-
         """
+        self.length = length
         self.augmentations = augmentations
         # encode augmentations as integer
         self.pheno2geno = {
@@ -19,11 +20,13 @@ class chromosome_generator:
     def __call__(self):
         # representation = random permutation and random intensity
         return [
-            (self.pheno2geno[k], random.uniform(*self.augmentations[k]))
-            for k in random.sample(list(self.augmentations.keys()), len(self.augmentations))[:len]
+            # gen a float or int based on range types
+            (k, random.uniform(*self.augmentations[k]) if isinstance(self.augmentations[k][0], float) else random.randint(*self.augmentations[k]))
+            for k in random.sample(list(self.augmentations.keys()), self.length)
         ]
 
 if __name__ == "__main__":
     c = chromosome_generator()
     print(c())
+    import pdb;pdb.set_trace()
 
--- essl/fitness.py
+++ essl/fitness.py
@@ -2,11 +2,45 @@ import numpy as np
 from torch import nn
 import torchvision
 import torch
-
+import torch.optim as optim
 from lightly.data import LightlyDataset
 from lightly.data import SimCLRCollateFunction, BaseCollateFunction
 from lightly.loss import NTXentLoss
 from lightly.models.modules import SimCLRProjectionHead
+from functools import partial
+import PIL
+import pandas as pd
+import os
+from sklearn.metrics import accuracy_score
+
+import ops
+import chromosome
+
+
+class SimCLR(nn.Module):
+    def __init__(self, backbone, out_features):
+        super().__init__()
+        self.backbone = backbone
+        self.projection_head = SimCLRProjectionHead(out_features, 512, 128)
+
+    def forward(self, x):
+        x = self.backbone(x).flatten(start_dim=1)
+
+        z = self.projection_head(x)
+        return z
+
+class finetune_model(nn.Module):
+    def __init__(self, backbone, in_features, num_outputs):
+        super().__init__()
+        self.backbone = backbone
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(in_features=in_features, out_features=num_outputs, bias=True),
+        )
+    def forward(self, x):
+        x = self.backbone(x).flatten(start_dim=1)
+        x = self.classifier(x)
+        return x
+
 
 def dummy_eval(chromosome):
     """
@@ -18,41 +52,34 @@ def dummy_eval(chromosome):
     opt = list(range(len(permutation)))
     return sum(np.array(opt) == np.array(permutation))
 
-def gen_augmentation(chromosome: list) -> torchvision.transforms.Compose:
+def gen_augmentation_torch(chromosome: list) -> torchvision.transforms.Compose:
     # gen augmentation
     # dataloader(augmentation)
-    return
+    aug = torchvision.transforms.Compose([
+        torchvision.transforms.Lambda(ops.__dict__[op](intensity=i))
+        for op, i in chromosome
+    ]+[torchvision.transforms.ToTensor()])
+    return aug
 
+def gen_augmentation_PIL(chromosome: list) -> torchvision.transforms.Compose:
+    # gen augmentation
+    # dataloader(augmentation)
+    aug = torchvision.transforms.Compose([
+        torchvision.transforms.Lambda(ops.__dict__[op](intensity=i))
+        for op, i in chromosome
+    ])
+    return aug
 
-
-class SimCLR(nn.Module):
-    def __init__(self, backbone, in_features):
-        super().__init__()
-        self.backbone = backbone
-        self.projection_head = SimCLRProjectionHead(in_features, 512, 128)
-
-    def forward(self, x):
-        x = self.backbone(x).flatten(start_dim=1)
-        z = self.projection_head(x)
-        return z
-
-class finetune_model(nn.Module):
-    def __init__(self, backbone, in_features):
-        super().__init__()
-        self.backbone = backbone
-        self.classifier = torch.nn.Sequential(
-            torch.nn.Linear(in_features=self.cin_features, out_features=num_outputs, bias=True),
-        )
-
-
-
-def ssl_representation(aug,dataset, num_epochs, device):
+def ssl_representation(aug: torchvision.transforms.Compose,
+                       dataset: LightlyDataset,
+                       num_epochs: int,
+                       device: str):
     # return features
     # model
     # call ssl training
     # different ssl algorithms
-    backbone = torchvision.models.resnet18()
-    model = SimCLR(backbone, in_features=backbone.fc.in_features)
+    backbone = torchvision.models.resnet18().to(device)
+    model = SimCLR(backbone, out_features=backbone.fc.out_features).to(device)
 
     # custom colate function -> basecollate
     collate_fn = BaseCollateFunction(aug)
@@ -64,7 +91,7 @@ def ssl_representation(aug,dataset, num_epochs, device):
         drop_last=True,
         num_workers=8,
     )
-    criterion = NTXentLoss()
+    criterion = NTXentLoss().to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.06)
     losses = []
     print("Starting Training")
@@ -83,23 +110,144 @@ def ssl_representation(aug,dataset, num_epochs, device):
         avg_loss = total_loss / len(dataloader)
         print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
         losses.append(float(avg_loss))
+    return model
 
 
+def visualize_chromosomes(save_path, num_samples=5):
+    c = chromosome.chromosome_generator()
+    cifar10 = torchvision.datasets.CIFAR10("datasets/cifar10", download=True)
+    sample = PIL.Image.fromarray(cifar10.data[7])
+    sample.save(os.path.join(save_path, "original.jpg"))
+
+    chromosomes = []
+    for i in range(num_samples):
+        cc = c()
+        chromosomes.append(cc)
+        aug = gen_augmentation_PIL(cc)
+        augmented_im = aug(sample)
+        augmented_im.save(os.path.join(save_path, f"{i}.jpg"))
+    df = pd.DataFrame(chromosomes, columns=[f"op{i}" for i in range(len(chromosomes[0]))])
+    df.to_csv(os.path.join(save_path, "chromosomes.csv"))
 
 
+def finetune_features(model: torch.nn.Module,
+                       train_data: torch.utils.data.Dataset,
+                       test_data: torch.utils.data.Dataset,
+                       num_epochs: int,
+                       device: str,
+                       num_outputs: int,
+                       batch_size: int = 32):
+    """
+    COMPONENTS WE MUST SPECIFY FOR FINETUNE:
+        - transform pipeline
+        - optimizer
+        - loss function
 
-def finetune_features(model):
+    EVALUATION:
+        - test acc?
+        - test auc?
+    :param model:
+    :param dataset:
+    :param num_epochs:
+    :param device:
+    :param batch_size:
+    :return:
+    """
     # finetuning or clustering
     # number of layers, number of neurons
     #
     backbone = model.backbone
-    return
+    model = finetune_model(backbone, backbone.fc.out_features, num_outputs).to(device)
+    trainloader = torch.utils.data.DataLoader(train_data,
+                                             batch_size=batch_size)
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    # train #
+    for epoch in range(num_epochs):
+        for X, y in trainloader:
+            inputs, labels = X.to(device), y.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    # evaluate #
+    testloader = torch.utils.data.DataLoader(test_data,
+                                              batch_size=batch_size)
+    model.eval()
+    y_true = torch.tensor([], dtype=torch.long).to(device)
+    pred_probs = torch.tensor([]).to(device)
+    # deactivate autograd engine
+    with torch.no_grad():
+        running_loss = 0.0
+        for X, y in testloader:
+            inputs = X.to(device)
+            labels = y.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item()
+            y_true = torch.cat((y_true, labels), 0)
+            pred_probs = torch.cat((pred_probs, outputs), 0)
+
+    y_true = y_true.cpu().numpy()
+    _, y_pred = torch.max(pred_probs, 1)
+    y_pred = y_pred.cpu().numpy()
+    # return acc
+    return accuracy_score(y_true, y_pred)
 
 def eval_chromosome(chromosome: list):
     aug = gen_augmentation(chromosome)
     features = ssl_representation(aug)
     return finetune_features(features)
 
+class eval_finetune:
+    """
+    proposed approach:
+    wrap above workflow in a classs to store global aspects of the evaluation such as
+    dataset and hparams
+    """
+    def __init__(self,
+                 dataset,
+                 backbone,
+                 ssl_method,
+                 ssl_epochs,
+                 finetune_opt,
+                 finetune_transform,
+                 finetune_epochs,
+                 finetune_loss):
+        pass
+
+
+if __name__ == "__main__":
+    c = chromosome.chromosome_generator()
+    cc = c()
+    aug = gen_augmentation_torch(cc)
+    cifar10 = torchvision.datasets.CIFAR10("datasets/cifar10", download=True)
+    dataset = LightlyDataset.from_torch_dataset(cifar10)
+    ssl_rep = ssl_representation(aug, dataset, num_epochs=1, device="cuda")
+
+    transform = torchvision.transforms.Compose(
+        [
+            # torchvision.transforms.ToPILImage(),
+            torchvision.transforms.Lambda(lambda image: image.convert('RGB')),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Resize((32, 32)),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    train_data = torchvision.datasets.CIFAR10("datasets/cifar10", download=True,
+                                           transform=transform, train=True)
+    test_data = torchvision.datasets.CIFAR10("datasets/cifar10", download=True,
+                                              transform=transform, train=False)
+
+
+    print("ACC: ", finetune_features(ssl_rep,
+                        train_data,
+                        test_data,
+                        num_epochs=1,
+                        device="cuda",
+                        num_outputs=10,
+                        batch_size=32))
 
 
 
