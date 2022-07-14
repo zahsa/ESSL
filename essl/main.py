38d4648a9223514fd84cf002db594d3af9e7db1d
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-07-14 12:42:20 -0300

    new mutation method where genes are chosen to be mutated randomly

--- essl/main.py
+++ essl/main.py
@@ -228,7 +228,7 @@ def main(pop_size, num_generations,
                 raise ValueError(f"invalid adaptive_pb value: {adaptive_pb}")
         # Apply crossover and mutation on the offspring
         # split list in two
-        parents = []
+        children = []
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
             if adaptive_pb == "AGA":
                 f_p = max([child1.fitness.values[0], child2.fitness.values[0]])
@@ -238,19 +238,21 @@ def main(pop_size, num_generations,
                     cxpb = 1
 
             if random.random() < cxpb:
-                parents.append(child1.id)
-                parents.append(child2.id)
+
                 toolbox.mate(child1, child2)
                 # generate new ids for children
                 child1.id = next(id_gen)
                 child2.id = next(id_gen)
+                children.append(child1.id)
+                children.append(child2.id)
                 del child1.fitness.values
                 del child2.fitness.values
+
 
         for mutant in offspring:
             if adaptive_pb == "AGA":
                 # if child was just created this round, mutate
-                if mutant.id in parents:
+                if mutant.id in children:
                     continue
                 # modify mutpb
                 if mutant.fitness.values[0] >= mean:
