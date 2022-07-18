800a998091b068ae4d4c0543d4bce15eea01fe66
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-07-18 15:58:16 -0300

    fix both original and choice gaussian mutation

--- essl/mutate.py
+++ essl/mutate.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from collections import Sequence
 
-def mutGaussian(individual,  mu=0, sigma=1, indpb=0.05, seed=10, discrete=False, intensity_increments=10):
+def mutGaussian(individual, indpb=0.05, discrete=False, intensity_increments=10):
     """
     DIrectly modified from source code to work with our chromosomes
 
@@ -20,54 +20,59 @@ def mutGaussian(individual,  mu=0, sigma=1, indpb=0.05, seed=10, discrete=False,
     :term:`sequence` individual composed of real valued attributes.
     The *indpb* argument is the probability of each attribute to be mutated.
     :param individual: Individual to be mutated.
-    :param mu: Mean or :term:`python:sequence` of means for the
-               gaussian addition mutation.
-    :param sigma: Standard deviation or :term:`python:sequence` of
-                  standard deviations for the gaussian addition mutation.
     :param indpb: Independent probability for each attribute to be mutated.
     :returns: A tuple of one individual.
     This function uses the :func:`~random.random` and :func:`~random.gauss`
     functions from the python base :mod:`random` module.
     """
-    random.seed(seed)
     size = len(individual)
-    import pdb;pdb.set_trace()
-    if not isinstance(mu, Sequence):
-        mu = repeat(mu, size)
-    elif len(mu) < size:
-        raise IndexError("mu must be at least the size of individual: %d < %d" % (len(mu), size))
-    if not isinstance(sigma, Sequence):
-        sigma = repeat(sigma, size)
-    elif len(sigma) < size:
-        raise IndexError("sigma must be at least the size of individual: %d < %d" % (len(sigma), size))
     if discrete:
-        for i, m, s in zip(range(size), mu, sigma):
+        for i in range(size):
             if random.random() < indpb:
                 i_range = DEFAULT_OPS[individual[i][0]]
                 increment = abs(i_range[1] - i_range[0]) / intensity_increments
                 if isinstance(i_range[0], int):
-                    # update = int(random.choice(np.arange(*i_range, increment, dtype=int)))
-                    update =  int((random.choice([-1, 1]) * increment) + individual[i][1])
+                    update =  int(individual[i][1] + (random.choice([-1, 1]) * increment))
+                    if update < i_range[0]:
+                        update = int(individual[i][1] + abs(increment))
+                    elif update > i_range[1]:
+                        update = int(individual[i][1] - increment)
                 else:
-                    # update = float(round(random.choice(np.arange(*i_range, increment, dtype=float)), 2))
                     update = float(round((random.choice([-1, 1]) * increment) + individual[i][1], 2))
-                if update <= DEFAULT_OPS[individual[i][0]][1] and update >= DEFAULT_OPS[individual[i][0]][0]:
-                    individual[i][1] = update
+                    if update < i_range[0]:
+                        update = float(round(individual[i][1] + abs(increment), 2))
+                    elif update > i_range[1]:
+                        update = float(round(individual[i][1] - increment, 2))
+                # if update <= DEFAULT_OPS[individual[i][0]][1] and update >= DEFAULT_OPS[individual[i][0]][0]:
+                individual[i][1] = update
     else:
-        for i, m, s in zip(range(size), mu, sigma):
+        for i in range(size):
             if random.random() < indpb:
-                update = individual[i][1] + random.gauss(m, s)
-                # max out range
-                if update < DEFAULT_OPS[individual[i][0]][0]:
-                    update = DEFAULT_OPS[individual[i][0]][0]
-                elif update > DEFAULT_OPS[individual[i][0]][1]:
-                    update = DEFAULT_OPS[individual[i][0]][1]
+                i_range = DEFAULT_OPS[individual[i][0]]
+                if isinstance(i_range[0], int):
+                    update = (random.choice([-1, 1]) + individual[i][1])
+                    # max out range
+                    if update < DEFAULT_OPS[individual[i][0]][0]:
+                        update = individual[i][1] + 1
+                    elif update > DEFAULT_OPS[individual[i][0]][1]:
+                        update = individual[i][1] - 1
+                else:
+                    m = abs(i_range[1] - i_range[0]) / 4
+                    s = m / 2
+                    sign = random.choice([1, -1])
+                    gauss = random.gauss(m, s)*sign
+                    update = individual[i][1] + gauss
+                    # max out range
+                    if update < DEFAULT_OPS[individual[i][0]][0]:
+                        update = individual[i][1] + abs(gauss)
+                    elif update > DEFAULT_OPS[individual[i][0]][1]:
+                        update = individual[i][1] - gauss
                 individual[i][1] = update
     return individual,
 
 
 
-def mutGaussianChoice(individual,  mu=0, sigma=1, discrete=False, intensity_increments=10):
+def mutGaussianChoice(individual, discrete=False, intensity_increments=10):
     """
     Randomly selects 1-N genes to mutate and applies mutation
     if discrete, increments in either negative or positive direction by increment size
@@ -92,13 +97,22 @@ def mutGaussianChoice(individual,  mu=0, sigma=1, discrete=False, intensity_incr
             i_range = DEFAULT_OPS[individual[i][0]]
             if isinstance(i_range[0], int):
                 update = (random.choice([-1, 1]) + individual[i][1])
+                # max out range
+                if update < DEFAULT_OPS[individual[i][0]][0]:
+                    update = individual[i][1] + 1
+                elif update > DEFAULT_OPS[individual[i][0]][1]:
+                    update = individual[i][1] - 1
             else:
-                update = individual[i][1] + random.gauss(mu, sigma)
-            # max out range
-            if update < DEFAULT_OPS[individual[i][0]][0]:
-                update = DEFAULT_OPS[individual[i][0]][0]
-            elif update > DEFAULT_OPS[individual[i][0]][1]:
-                update = DEFAULT_OPS[individual[i][0]][1]
+                m = abs(i_range[1] - i_range[0]) / 4
+                s = m / 2
+                sign = random.choice([1, -1])
+                gauss = random.gauss(m, s) * sign
+                update = individual[i][1] + gauss
+                # max out range
+                if update < DEFAULT_OPS[individual[i][0]][0]:
+                    update = individual[i][1] + abs(gauss)
+                elif update > DEFAULT_OPS[individual[i][0]][1]:
+                    update = individual[i][1] - gauss
             individual[i][1] = update
 
     return individual,
@@ -106,7 +120,9 @@ def mutGaussianChoice(individual,  mu=0, sigma=1, discrete=False, intensity_incr
 
 
 
-
-
-
-
+if __name__ == "__main__":
+    from essl.chromosome import chromosome_generator
+    cc = chromosome_generator()
+    chromo = cc()
+    for _ in range(10):
+        print(mutGaussianChoice(chromo, discrete=False))
