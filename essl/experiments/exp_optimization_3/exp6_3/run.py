6c5780493e936558566fa31fc8eae9b602b7b0c9
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-08-11 10:16:37 -0300

    add exps

---
+++ essl/experiments/exp_optimization_3/exp6_3/run.py
@@ -0,0 +1,63 @@
+import random
+import os
+from essl.GA import GA, GA_mo
+
+num_seeds = 3
+pop_size = 15
+num_generations = 10
+cxpb = 0.8
+mutpb = 0.8
+crossover = "PMX"
+selection = "roulette"
+dataset = "Cifar10"
+backbone = "largerCNN_backbone"
+ssl_task = "SimSiam"
+ssl_epochs = 10
+ssl_batch_size = 32
+evaluate_downstream_method = "finetune"
+evaluate_downstream_kwargs = { }
+device = "cuda"
+exp_dir = "./"
+use_tensorboard = True
+save_plots = True
+chromosome_length = 3
+num_elite = 2
+adaptive_pb = "AGA"
+patience = -1
+discrete_intensity = False
+eval_method = "best val test"
+
+if __name__ == "__main__":
+    for seed in random.sample(range(10), num_seeds):
+        exp_seed_dir = os.path.join(exp_dir, str(seed))
+        if not os.path.isdir(exp_seed_dir):
+            os.mkdir(exp_seed_dir)
+
+        # save environment
+        os.system(f"pip freeze > {os.path.join(exp_seed_dir, 'env.txt')}")
+        GA(
+            pop_size=pop_size,
+            num_generations=num_generations,
+            cxpb=cxpb,
+            mutpb=mutpb,
+            crossover=crossover,
+            selection=selection,
+            dataset=dataset,
+            backbone=backbone,
+            ssl_task=ssl_task,
+            ssl_epochs=ssl_epochs,
+            ssl_batch_size=ssl_batch_size,
+            evaluate_downstream_method=evaluate_downstream_method,
+            evaluate_downstream_kwargs=evaluate_downstream_kwargs,
+            device=device,
+            exp_dir=exp_dir,
+            use_tensorboard=use_tensorboard,
+            save_plots=save_plots,
+            chromosome_length=chromosome_length,
+            seed=seed,
+            num_elite=num_elite,
+            adaptive_pb=adaptive_pb,
+            patience=patience,
+            discrete_intensity=discrete_intensity,
+            eval_method=eval_method
+           )
