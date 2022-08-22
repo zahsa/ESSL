8a79adb42bbf5394f297b56d2bb27ce6cbb5abf9
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-08-22 18:11:31 -0300

    add runexps script

---
+++ essl/experiments/generated_exps/run_Exps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_1/exp8_1.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_2/exp8_2.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_3/exp8_3.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_0/exp9_0.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_1/exp9_1.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_2/exp9_2.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_3/exp9_3.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_0/exp6_0.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_1/exp6_1.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_2/exp6_2.sh
+sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_3/exp6_3.sh
