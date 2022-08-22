b2fa0b0c0eb8d8b7839c2de01c75146d6963ff8e
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-08-22 12:12:09 -0300

    fix problem with generated exps

--- essl/experiments/generated_exps/exp_scripts/exp_essl_2/exp4_0/run.py
+++ essl/experiments/generated_exps/exp_scripts/exp_essl_2/exp4_0/run.py
@@ -69,28 +69,5 @@ if __name__ == "__main__":
         # save environment
         os.system(f"pip freeze > {os.path.join(exp_seed_dir, 'env.txt')}")
         GA_mo(
-            pop_size = 15,
-            num_generations = 10,
-            cxpb1 = 0.8,
-            mutpb1 = 0.8,
-            cxpb2 = 0.5,
-            mutpb2 = 0.2,
-            dataset = 'Cifar10',
-            backbone = 'largerCNN_backbone',
-            ssl_epochs = 10,
-            ssl_batch_size = 32,
-            evaluate_downstream_method = 'finetune',
-            device = 'cuda',
-            exp_dir = './',
-            use_tensorboard = True,
-            save_plots = True,
-            crossover = 'PMX',
-            chromosome_length = 3,
-            selection = 'roulette',
-            num_seeds = 3,
-            adaptive_pb1 = 'AGA',
-            eval_method = 'best val test',
-            ssl_tasks = 'v6',
-            num_elite = 2,
             
            )
--- essl/experiments/generated_exps/exp_scripts/exp_optimization_3/exp6_0/run.py
+++ essl/experiments/generated_exps/exp_scripts/exp_optimization_3/exp6_0/run.py
@@ -65,26 +65,5 @@ if __name__ == "__main__":
         # save environment
         os.system(f"pip freeze > {os.path.join(exp_seed_dir, 'env.txt')}")
         GA(
-            pop_size = 15,
-            num_generations = 10,
-            cxpb = 0.8,
-            mutpb = 0.8,
-            dataset = 'Cifar10',
-            backbone = 'largerCNN_backbone',
-            ssl_task = 'NNCLR',
-            ssl_epochs = 10,
-            ssl_batch_size = 32,
-            evaluate_downstream_method = 'finetune',
-            device = 'cuda',
-            exp_dir = './',
-            use_tensorboard = True,
-            save_plots = True,
-            crossover = 'PMX',
-            chromosome_length = 3,
-            selection = 'roulette',
-            num_seeds = 10,
-            adaptive_pb = 'AGA',
-            eval_method = 'best val test',
-            num_elite = 2,
             
            )
