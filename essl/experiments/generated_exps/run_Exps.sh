678a5335c693891829324919c23221afa7116a0f
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-08-23 12:35:16 -0300

    update run_exp.sh

--- essl/experiments/generated_exps/run_Exps.sh
+++ essl/experiments/generated_exps/run_Exps.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_0/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_0/opt3_exp8_0.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_1/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_1/opt3_exp8_1.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_2/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_2/opt3_exp8_2.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_3/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_3/opt3_exp8_3.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_0/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_0/opt3_exp9_0.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_1/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_1/opt3_exp9_1.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_2/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_2/opt3_exp9_2.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_3/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_3/opt3_exp9_3.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_0/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_0/opt3_exp6_0.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_1/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_1/opt3_exp6_1.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_2/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_2/opt3_exp6_2.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_3/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_3/opt3_exp6_3.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp4_0/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp4_0/essl2_exp4_0.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp5/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp5/essl2_exp5.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp6/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp6/essl2_exp6.sh
+chmod +x /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp7/run.sh
 sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_essl_2/exp7/essl2_exp7.sh
