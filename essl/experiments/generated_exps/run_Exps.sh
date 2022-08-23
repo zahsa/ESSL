e24c48463f6ee4ef4424c7babb66d15e761d8a44
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-08-23 10:28:15 -0300

    fix run_exps.sh

--- essl/experiments/generated_exps/run_Exps.sh
+++ essl/experiments/generated_exps/run_Exps.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_1/exp8_1.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_2/exp8_2.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp8_3/exp8_3.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_0/exp9_0.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_1/exp9_1.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_2/exp9_2.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp9_3/exp9_3.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_0/exp6_0.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_1/exp6_1.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_2/exp6_2.sh
-sbatch /home/zahras/ESSL/essl/experiments/generated_exps/CC_scripts/exp_optimization_3/exp6_3/exp6_3.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp8_1/exp8_1.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp8_2/exp8_2.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp8_3/exp8_3.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp9_0/exp9_0.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp9_1/exp9_1.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp9_2/exp9_2.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp9_3/exp9_3.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp6_0/exp6_0.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp6_1/exp6_1.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp6_2/exp6_2.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_optimization_3/exp6_3/exp6_3.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_essl2/exp4_0/exp4_0.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_essl2/exp5/exp5.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_essl2/exp6/exp6.sh
+sbatch /home/zahras/scratch/CC_scripts/exp_essl2/exp7/exp7.sh
