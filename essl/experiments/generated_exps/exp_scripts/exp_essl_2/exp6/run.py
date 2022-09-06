01ac5ae9a1a36416d7446afb2c4097724d55b3c2
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-09-06 09:42:00 -0300

    fix bootstrap plot saving issue

--- essl/experiments/generated_exps/exp_scripts/exp_essl_2/exp6/run.py
+++ essl/experiments/generated_exps/exp_scripts/exp_essl_2/exp6/run.py
@@ -72,28 +72,51 @@ if __name__ == "__main__":
         os.system(f"pip freeze > {os.path.join(exp_seed_dir, 'env.txt')}")
         GA_mo(
             seed=seed,
+            exp_dir=exp_seed_dir,
             pop_size = 15,
+                
             num_generations = 10,
+                
             cxpb1 = 0.8,
+                
             mutpb1 = 0.8,
+                
             cxpb2 = 0.5,
+                
             mutpb2 = 0.2,
+                
             dataset = 'Cifar10',
+                
             backbone = 'largerCNN_backbone',
+                
             ssl_epochs = 10,
+                
             ssl_batch_size = 32,
+                
             evaluate_downstream_method = 'finetune',
+                
             device = 'cuda',
-            exp_dir = './',
+                
+            
             use_tensorboard = True,
+                
             save_plots = True,
+                
             crossover = 'PMX',
+                
             chromosome_length = 3,
+                
             selection = 'roulette',
+                
             adaptive_pb1 = 'AGA',
+                
             eval_method = 'best val test',
+                
             ssl_tasks = 'v6',
+                
             num_elite = 2,
+                
             aug_ops = 'DEFAULT_OPS',
+                
             
            )
--- essl/experiments/generated_exps/exp_scripts/exp_optimization_3/exp8_1/run.py
+++ essl/experiments/generated_exps/exp_scripts/exp_optimization_3/exp8_1/run.py
@@ -68,26 +68,47 @@ if __name__ == "__main__":
         os.system(f"pip freeze > {os.path.join(exp_seed_dir, 'env.txt')}")
         GA(
             seed=seed,
+            exp_dir=exp_seed_dir,
             pop_size = 15,
+                
             num_generations = 10,
+                
             cxpb = 0.8,
+                
             mutpb = 0.8,
+                
             dataset = 'Cifar10',
+                
             backbone = 'largerCNN_backbone',
+                
             ssl_task = 'BYOL',
+                
             ssl_epochs = 10,
+                
             ssl_batch_size = 256,
+                
             evaluate_downstream_method = 'finetune',
+                
             device = 'cuda',
-            exp_dir = './',
+                
+            
             use_tensorboard = True,
+                
             save_plots = True,
+                
             crossover = 'PMX',
+                
             chromosome_length = 3,
+                
             selection = 'roulette',
+                
             adaptive_pb = 'AGA',
+                
             eval_method = 'best val test',
+                
             num_elite = 2,
+                
             aug_ops = 'OPS_NO_FLIP',
+                
             
            )
