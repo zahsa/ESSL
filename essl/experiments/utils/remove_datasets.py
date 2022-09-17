ac060c2aab8721b36095d22f9d9c241f791e0a3f
Noah <noah@bigdata-gpu1.research.cs.dal.ca>
2022-09-17 16:41:07 -0300

    add SVHN experiments

--- essl/experiments/utils/remove_datasets.py
+++ essl/experiments/utils/remove_datasets.py
@@ -10,7 +10,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
     exps = glob.glob(os.path.join(args.exp_dir, "*exp*"))
     for e in exps:
+<<<<<<< HEAD
         seed_directories = [d for d in glob.glob(os.path.join(e, "*/"))]
+=======
+        seed_directories = [d for d in glob.glob(os.path.join(e, "*/")) if "tensorboard" not in d and "datasets" not in d and "models" not in d]
+>>>>>>> 66edcba5ab86ee125088d017ac1065134a34cdbe
         for dir in seed_directories:
             new_dir = os.path.join(args.targ_dir, dir.split(args.exp_dir)[1][1:])
             shutil.copytree(dir, new_dir, ignore=shutil.ignore_patterns("*tensorboard*", "*datasets*", "*models*"))
