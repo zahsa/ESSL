f28c1e2422250b92b93f4ec04daed4b22ebadcbf
Noah Barrett <48027601+NoahBarrett98@users.noreply.github.com>
2022-10-11 11:24:53 -0300

    Update run.py

--- essl/experiments/generated_exps/exp_scripts/exp_optimization_3/exp8_3/run.py
+++ essl/experiments/generated_exps/exp_scripts/exp_optimization_3/exp8_3/run.py
@@ -24,7 +24,7 @@ num_seeds = 10
 adaptive_pb = 'AGA'
 eval_method = 'best val test'
 num_elite = 2
-aug_ops = 'OPS_NO_FLIP'
+aug_ops = 'DEFAULT_OPS'
 
 
 args = {
@@ -49,7 +49,7 @@ args = {
     'adaptive_pb':'AGA',
     'eval_method':'best val test',
     'num_elite':2,
-    'aug_ops':'OPS_NO_FLIP',
+    'aug_ops':'DEFAULT_OPS',
     
     }
 
